87ccca37ce4b8c40335335b94657cff91f1e8208
betalantz <betalantz@gmail.com>
2025-09-04 09:29:45 -0700

    small clean up

--- client/src/components/ui/color-mode.tsx
+++ client/src/components/ui/color-mode.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import * as React from "react";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ThemeProvider as NextThemesProvider, useTheme as useNextTheme } from "next-themes";
